18f34e3a463a43b5ae6f0e5c90f4230c55c2cc60
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-01-31 20:30:31 +0500

    about desktop mobile tayyor faqat accardion tayyor emas

--- js/main.js
+++ js/main.js
@@ -20,20 +20,24 @@ select_items2.forEach(item => {
 // location select
 
 // footer accardion
-const accSingleTriggers = document.querySelectorAll('.js-acc-single-trigger');
+try {
+  const accSingleTriggers = document.querySelectorAll('.js-acc-single-trigger');
 
-accSingleTriggers.forEach(trigger => trigger.addEventListener('click', toggleAccordion));
-
-function toggleAccordion() {
-  const items = document.querySelectorAll('.js-acc-item');
-  const thisItem = this.parentNode;
-
-  items.forEach(item => {
-    if (thisItem == item) {
-      thisItem.classList.toggle('is-open');
-      return;
-    }
-    item.classList.remove('is-open');
-  });
+  accSingleTriggers.forEach(trigger => trigger.addEventListener('click', toggleAccordion));
+  
+  function toggleAccordion() {
+    const items = document.querySelectorAll('.js-acc-item');
+    const thisItem = this.parentNode;
+  
+    items.forEach(item => {
+      if (thisItem == item) {
+        thisItem.classList.toggle('is-open');
+        return;
+      }
+      item.classList.remove('is-open');
+    });
+  }
+} catch(err) {
+  console.log(err);
 }
 // footer accardion
